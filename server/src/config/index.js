bbb14a12296cf72e6e7d24bb0fc4e3a226ce4ffd
Shane Israel <shanemisrael@gmail.com>
2021-09-12 22:02:24 -0600

    add config, update start scripts

---
+++ server/src/config/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    tmdb_api_key: process.env.TMDB_API_KEY,
+    tmdb_read_access_token: process.env.TMDB_READ_ACCESS_TOKEN,
+    appdata: process.env.LOCALAPPDATA ? process.env.APPDATA + '/Apollo' : (process.platform == 'darwin' ? process.env.HOME + '/Library/Apollo' : process.env.HOME + "/.apollo"),
+    imageDir: '/images',
+    logsDir: '/logs',
+    dbname: 'dev.sqlite'
+  },
+  production: {
+    tmdb_api_key: process.env.TMDB_API_KEY,
+    tmdb_read_access_token: process.env.TMDB_READ_ACCESS_TOKEN,
+    appdata: process.env.LOCALAPPDATA ? process.env.APPDATA + '/Apollo' : (process.platform == 'darwin' ? process.env.HOME + '/Library/Apollo' : process.env.HOME + "/.apollo"),
+    imageDir: '/images',
+    logsDir: '/logs',
+    dbname: 'apollo.db'
+  }
+}
