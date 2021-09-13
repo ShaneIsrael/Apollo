3a427de6891379244bf5df8cd05066b265d6485c
Shane Israel <shanemisrael@gmail.com>
2021-09-12 18:14:46 -0600

    fix pkg unable to find modules

--- server/src/logger/index.js
+++ server/src/logger/index.js
@@ -1,8 +1,8 @@
 const { transports, createLogger, format } = require('winston')
 const { combine, splat, timestamp, printf } = format
 const path = require('path')
-const environment = process.env.NODE_ENV
-const config = require('../config')[environment || 'development']
+const environment = process.env.NODE_ENV || 'production'
+const config = require(path.join(__dirname, '../config'))[environment]
 // define the custom settings for each transport (file, console)
 const options = {
   file: {
